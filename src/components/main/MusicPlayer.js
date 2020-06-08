4f8f7a88d3e45276d900d389499c504973ec05b9
socratone <gim2origin@gmail.com>
2020-06-08 20:21:22 +0900

    Rename variables to MusicPlayer.js

--- src/components/main/MusicPlayer.js
+++ src/components/main/MusicPlayer.js
@@ -4,13 +4,13 @@ import RatingForm from './RatingForm';
 import MusicTitle from './MusicTitle';
 import MusicProgressBar from './MusicProgressBar';
 import MusicNavBar from './MusicNavBar';
+import MusicQueue from './MusicQueue';
 import '../../styles/MusicPlayer.scss';
 
 import getRecommendedPlaylist from '../../lib/apis/getRecommendedPlaylist';
 
 const MusicPlayer = (props) => {
-  const queue = props.recommended.items; // fakeData 로딩
-  const firstItem = queue[0].snippet.resourceId.videoId; // fakeData 로딩
+  const { currentItems, currentItem } = props; // fakeData 로딩
   const [player, setPlayer] = useState(null); // video를 처리하기 위한 player 변수
   const opts = {
     playerVars: {
@@ -19,28 +19,24 @@ const MusicPlayer = (props) => {
   };
   const onReady = (event) => {
     setPlayer(event.target); // video가 로딩 됐을 때 player 변수로 video를 참조할 수 있게 해준다.
-  }
+  };
   return (
     <div className="player">
       <div id="player-window" className="big-window">
         <YouTube
-          videoId={firstItem}
+          videoId={currentItem.snippet.resourceId.videoId}
           opts={opts}
           onReady={onReady}
-          className="player-video" />
+          className="player-video"
+        />
         <RatingForm />
         <MusicTitle />
         <MusicProgressBar />
-        {/* <button onClick={() => {
-          getRecommendedPlaylist(10)
-            .then(res => res.json())
-            .then(json => console.log(JSON.stringify(json, null, 2)))
-            .catch(err => console.log(err));
-        }}>temporary</button> */}
-        <MusicNavBar player={player} />
+        <MusicQueue currentItems={currentItems} />
       </div>
+      <MusicNavBar player={player} />
     </div>
   );
-}
+};
 
 export default MusicPlayer;
