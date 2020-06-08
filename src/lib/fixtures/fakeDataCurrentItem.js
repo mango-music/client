2b19f23cb48fefe5f9a664d559b8a357b470f549
socratone <gim2origin@gmail.com>
2020-06-08 20:21:57 +0900

    Implement fakeData
     - current item
     - current item for mango server
     - recommended items
     - recommended items for mango server

---
+++ src/lib/fixtures/fakeDataCurrentItem.js
@@ -0,0 +1,47 @@
+const fakeDataCurrentItem = {
+  "kind": "youtube#playlistItem",
+  "etag": "cc-QOaoGtOf8_PLw_Qd4myM5mRk",
+  "id": "UExGZ3F1TG5MNTlhbEdKY2RjMEJFWkpiMnA3SWdrTDBPZS4xQzk3NTVFMUExQjY4RURB",
+  "snippet": {
+    "publishedAt": "2020-06-08T06:53:37Z",
+    "channelId": "UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+    "title": "Lady Gaga, Ariana Grande - Rain On Me (Official Music Video)",
+    "description": "“CHROMATICA”\nTHE SIXTH ALBUM BY LADY GAGA\nOUT NOW\nhttp://smarturl.it/Chromatica\n\n\"RAIN ON ME”\nTHE NEW SINGLE FROM LADY GAGA WITH ARIANA GRANDE\nOUT NOW\nhttp://smarturl.it/RainOnMeChromatica\n\nDIRECTED BY ROBERT RODRIGUEZ\n\nSHOP THE OFFICIAL CHROMATICA COLLECTION: http://smarturl.it/GagaStore\n\nFOLLOW LADY GAGA:\nFacebook: http://gaga.lk/facebook\nInstagram: http://gaga.lk/Instagram\nSnapchat: http://gaga.lk/Snapchat\nTwitter: http://gaga.lk/Twitter\nWebsite: http://www.ladygaga.com/\nOfficial Store: http://gaga.lk/GagaStore\nEmail List: http://gaga.lk/News\n\nFOLLOW ARIANA GRANDE:\nFacebook: http://ArianaGrande.lnk.to/FacebookYT\nInstagram: http://ArianaGrande.lnk.to/InstagramYT\nTwitter: http://ArianaGrande.lnk.to/TwitterYT\nWebsite: http://ArianaGrande.lnk.to/WebsiteYT\nOfficial Store: http://ArianaGrande.lnk.to/ShopYT\nEmail List: http://ArianaGrande.lnk.to/SignUpYT\n\nMusic video by Lady Gaga, Ariana Grande performing Rain On Me. © 2020 Interscope Records",
+    "thumbnails": {
+      "default": {
+        "url": "https://i.ytimg.com/vi/AoAm4om0wTs/default.jpg",
+        "width": 120,
+        "height": 90
+      },
+      "medium": {
+        "url": "https://i.ytimg.com/vi/AoAm4om0wTs/mqdefault.jpg",
+        "width": 320,
+        "height": 180
+      },
+      "high": {
+        "url": "https://i.ytimg.com/vi/AoAm4om0wTs/hqdefault.jpg",
+        "width": 480,
+        "height": 360
+      },
+      "standard": {
+        "url": "https://i.ytimg.com/vi/AoAm4om0wTs/sddefault.jpg",
+        "width": 640,
+        "height": 480
+      },
+      "maxres": {
+        "url": "https://i.ytimg.com/vi/AoAm4om0wTs/maxresdefault.jpg",
+        "width": 1280,
+        "height": 720
+      }
+    },
+    "channelTitle": "Music",
+    "playlistId": "PLFgquLnL59alGJcdc0BEZJb2p7IgkL0Oe",
+    "position": 9,
+    "resourceId": {
+      "kind": "youtube#video",
+      "videoId": "AoAm4om0wTs"
+    }
+  }
+};
+
+export default fakeDataCurrentItem;
