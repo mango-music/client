29f5c1e8b9cc9d953f998dc8b7e9a176230d3c3f
socratone <gim2origin@gmail.com>
2020-06-08 20:18:21 +0900

    Fix lint

--- src/components/main/MusicNavBar.js
+++ src/components/main/MusicNavBar.js
@@ -10,27 +10,35 @@ const MusicNavBar = (props) => {
         <button>←</button>
       </div>
       <div id="play-button" className="hidden">
-        <button onClick={() => {
-          props.player.playVideo();
-          document.getElementById('play-button').classList.toggle('hidden');
-          document.getElementById('pause-button').classList.toggle('hidden');
-        }}>
+        <button
+          onClick={() => {
+            props.player.playVideo();
+            document.getElementById('play-button').classList.toggle('hidden');
+            document.getElementById('pause-button').classList.toggle('hidden');
+          }}
+        >
           ▶️
         </button>
       </div>
       <div id="pause-button">
-        <button onClick={() => {
-          props.player.pauseVideo();
-          document.getElementById('pause-button').classList.toggle('hidden');
-          document.getElementById('play-button').classList.toggle('hidden');
-        }}>
+        <button
+          onClick={() => {
+            props.player.pauseVideo();
+            document.getElementById('pause-button').classList.toggle('hidden');
+            document.getElementById('play-button').classList.toggle('hidden');
+          }}
+        >
           ⏸
         </button>
       </div>
-      <div><button>→</button></div>
-      <div><button>⤺</button></div>
+      <div>
+        <button>→</button>
+      </div>
+      <div>
+        <button>⤺</button>
+      </div>
     </div>
   );
-}
+};
 
 export default MusicNavBar;
