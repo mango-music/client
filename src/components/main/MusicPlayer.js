2d618c10bc66c2d03fef7a10987551f61e0af70b
socratone <gim2origin@gmail.com>
2020-06-09 15:32:54 +0900

    Add progress bar to MusicPlayer
     - 영상의 재생 시간에 따라 움직이는 노드를 추가
     - 영상의 현재 재생 시간 표시
     - 영상의 길이 시간으로 표시

--- src/components/main/MusicPlayer.js
+++ src/components/main/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import RatingForm from './RatingForm';
 import MusicTitle from './MusicTitle';
@@ -7,13 +7,15 @@ import MusicNavBar from './MusicNavBar';
 import MusicQueue from './MusicQueue';
 import '../../styles/MusicPlayer.scss';
 
-import getRecommendedPlaylist from '../../lib/apis/getRecommendedPlaylist';
-
+let timer;
 const MusicPlayer = (props) => {
   const { currentItems, currentItem } = props; // fakeData 로딩
   const [player, setPlayer] = useState(null); // video를 처리하기 위한 player 변수
   const [itemIndex, setItemIndex] = useState(0); // 배열의 몇 번째 음악을 재생하는지 알려주는 state
   const [isPlayButtonOn, setIsPlayButtonOn] = useState(false); // 재생 버튼과 일시 정지 버튼을 위한 state
+  const [currentTime, setCurrentTime] = useState(0); // 플레이어가 재생하고 있는 시간
+  const [durationTime, setDurationTime] = useState(null); // 영상의 총 길이
+
   const opts = {
     playerVars: {
       autoplay: 1,
@@ -22,6 +24,24 @@ const MusicPlayer = (props) => {
   const onReady = (event) => {
     setPlayer(event.target); // video가 로딩 됐을 때 player 변수로 video를 참조할 수 있게 해준다.
   };
+  const handleStateChange = (event) => {
+    if (event.data == 1) {
+      // 재생중
+      timer = setInterval(() => {
+        const currentSecond = Math.floor(player.getCurrentTime());
+        if (currentSecond) {
+          setCurrentTime(currentSecond);
+        }
+      }, 1000);
+    } else if (event.data === 2 || event.data === 0) {
+      // 일시중지됨, 종료됨
+      clearInterval(timer);
+    }
+    if (event.data === -1 || event.data === 1 || event.data === 2) {
+      // 시작되지 않음, 재생중, 일시정지됨
+      setDurationTime(Math.floor(player.getDuration()));
+    }
+  };
   return (
     <div id="player-selector" className="big-player">
       <div className="player-window">
@@ -29,11 +49,15 @@ const MusicPlayer = (props) => {
           videoId={currentItem.snippet.resourceId.videoId}
           opts={opts}
           onReady={onReady}
+          onStateChange={handleStateChange}
           className="iframe-video"
         />
         <RatingForm />
         <MusicTitle title={currentItem.snippet.title} />
-        <MusicProgressBar />
+        <MusicProgressBar
+          currentTime={currentTime}
+          durationTime={durationTime}
+        />
         <MusicQueue
           currentItems={currentItems}
           setItemIndex={setItemIndex}
