2e8fcc7927611160177142ae81d71e1bb3b43ec2
socratone <gim2origin@gmail.com>
2020-06-15 11:13:46 +0900

    Add preventing duplicate play in search

--- src/components/main/SearchEntry.js
+++ src/components/main/SearchEntry.js
@@ -5,33 +5,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SearchEntry = (props) => {
   const { currentItems, setCurrentItem, setCurrentItems } = props;
+
   return (
     <li>
       <div className="search-image">
         <img src={props.thumbnail} />
       </div>
       <p
-        onClick={async () => {
+        onClick={() => {
           console.log(`${props.title}을 재생합니다.`);
-          const item = await getMusic();
-          if (item === 'isExsistent') {
-            // temp: 현재 절대 통과 안 됨
-            // 별점을 이미 매긴 음악이라면 서버의 것을 가져온다.
-            setCurrentItem(item);
-            setCurrentItems([item]);
-          } else {
-            // 그렇지 않다면 데이터 형태를 바꿔준다.
-            const item = {
-              title: props.title,
-              thumbnail: props.thumbnail,
-              videoid: props.videoid,
-              rating: 0, // 초기 rating은 0으로 한다?
-            };
-            const newCurrentItems = currentItems.slice();
-            newCurrentItems.unshift(item);
-            setCurrentItem(item);
-            setCurrentItems(newCurrentItems);
+          const newCurrentItems = [...currentItems];
+          const item = {
+            title: props.title,
+            thumbnail: props.thumbnail,
+            videoid: props.videoid,
+            rating: 0, // 초기 rating은 0으로 한다?
+          };
+          // currentItems에 중복된 비디오가 있다면 삭제
+          for (let i = 0; i < newCurrentItems.length; i++) {
+            if (newCurrentItems[i].videoid === item.videoid) {
+              newCurrentItems.splice(i, 1);
+              break;
+            }
           }
+          newCurrentItems.unshift(item);
+          setCurrentItem(item);
+          setCurrentItems(newCurrentItems);
         }}
       >
         {props.title}
