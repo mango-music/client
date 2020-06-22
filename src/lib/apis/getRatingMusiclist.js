ad0754afd9e9bca974bf7d8bf8e41996586efb1d
knu9910 <knu991@naver.com>
2020-06-22 14:00:25 +0900

    Create api module getRatingMusiclist

---
+++ src/lib/apis/getRatingMusiclist.js
@@ -0,0 +1,13 @@
+import apiHelper from './apiHelper';
+
+const getRatingMusiclist = async () => {
+  const url = "http://13.209.19.101:3000/getRatingMusiclist";
+  const res = await apiHelper(url);
+  console.log(res);
+  return res.data;
+}
+
+// getRatingMusiclist
+//   .then(data => console.log(data));
+// output = [{음악}, {음악}, {음악}]
+export default getRatingMusiclist;
