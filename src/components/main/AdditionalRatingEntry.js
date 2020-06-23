e952ad8b3284132d1283c930e6388c823632610a
socratone <gim2origin@gmail.com>
2020-06-23 17:21:10 +0900

    Implement AdditionalRating component

--- src/components/main/AdditionalRatingEntry.js
+++ src/components/main/AdditionalRatingEntry.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const AdditionalRatingEntry = () => {
-  return <li>1111</li>;
+const AdditionalRatingEntry = (props) => {
+  const { music } = props;
+  const src = `https://www.youtube.com/embed/${music.videoid}?autoplay=0`;
+  return (
+    <li>
+      <div className="iframe-wrapper">
+        <iframe
+          title={music.videoid}
+          type="text/html"
+          src={src}
+          frameBorder="0"
+        />
+      </div>
+      <div className="stars">★★★★★</div>
+      <p>{music.title}</p>
+    </li>
+  );
 };
 
 export default AdditionalRatingEntry;
