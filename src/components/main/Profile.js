cca73077553297b4432bdec2e14102a324ae0044
Lee-Gwang-soo <59168432+Lee-Gwang-soo@users.noreply.github.com>
2020-06-24 11:23:34 +0900

    profile nickname update, create api
      - 프로필 닉네임 변경기능 구현
      - 라우팅 에러 발생
      - 닉네임 변경 api 생성
      - 프로필 이미지 변경 구현예정

--- src/components/main/Profile.js
+++ src/components/main/Profile.js
@@ -1,10 +1,26 @@
+/* eslint-disable */
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import Signout from '../auth/Signout';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../../styles/Profile.scss';
+import changeName from '../../lib/apis/changeName';
 
-const Profile = ({ profile, handleLogout }) => {
+const Profile = (props) => {
+  const { profile, handleLogout, history } = props;
+  const a = async () => {
+    var userinfo = await changeName(
+      document.querySelector('.changeNick').value,
+    );
+    localStorage.setItem('x-user-info', JSON.stringify(userinfo));
+    console.log(userinfo);
+    history.push('/');
+  };
+
+  //기존에 있던 userinfo.nickname을 바꿔서 서버에 전송했음
+  //기존에 있던 userinfo.nickname을 바꾼 닉네임으로 바꿔서 다시 저장.
+
   return (
     <div id="profile">
       <header>
@@ -13,7 +29,16 @@ const Profile = ({ profile, handleLogout }) => {
       <div id="profile-image">
         <FontAwesomeIcon icon={faUser} color="#afafaf" />
       </div>
-
+      <div>
+        <input className="changeNick" />
+        <button
+          onClick={() => {
+            a();
+          }}
+        >
+          변경하기
+        </button>
+      </div>
       <div>{profile.id}</div>
       <div>{profile.email}</div>
 
@@ -24,4 +49,4 @@ const Profile = ({ profile, handleLogout }) => {
   );
 };
 
-export default Profile;
+export default withRouter(Profile);
