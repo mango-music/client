415a6a60c1c34213da46d27099b6a0af4b0cb548
socratone <gim2origin@gmail.com>
2020-06-27 20:46:55 +0900

    Remove fakeDatas

--- src/lib/fixtures/fkdtCurrentItem.js
+++
@@ -1,8 +0,0 @@
-const fkdtCurrentItem = {
-  "title": "[MV] IU(아이유) _ eight(에잇) (Prod.&Feat. SUGA of BTS)",
-  "thumbnails": "https://i.ytimg.com/vi/TgOu00Mf3kI/mqdefault.jpg",
-  "videoid": "TgOu00Mf3kI",
-  "rating": 4
-};
-
-export default fkdtCurrentItem;
